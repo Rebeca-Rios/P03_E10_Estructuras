96f9887e7d5818047f75195e45f7313c1c13bd9b
Rebeca-Rios <80608872+Rebeca-Rios@users.noreply.github.com>
2021-03-24 01:38:59 -0600

    Update polinomio.c

--- polinomio.c
+++ polinomio.c
@@ -1,30 +1,29 @@
-//ESTE ES EL QUE FUNCIONA
 #include "polinomio.h"
 #include <stdlib.h>
 #include <locale.h>
 
-/*Esta función pregunta al usuario el grado del polinomio y reserva espacio
+/*Esta funciÃ³n pregunta al usuario el grado del polinomio y reserva espacio
   de acuerdo a lo que conteste*/
 POLI crear(){
 	POLI a; //Se declara el polinomio con el tipo de la estructura
-	int grado; //Auxiliar para guardar los términos del polinomio
-	printf("\n\t\t¿Cuál es el grado del polinomio? ");
+	int grado; //Auxiliar para guardar los tÃ©rminos del polinomio
+	printf("\n\t\tÂ¿CuÃ¡l es el grado del polinomio? ");
 	scanf("%d", &grado);
 	
-	//A los términos se le suma uno por que si el polinomio es grado 2, no está contando el elemento x^0
+	//A los tÃ©rminos se le suma uno por que si el polinomio es grado 2, no estÃ¡ contando el elemento x^0
 	a.terminos = grado + 1;
 	
-	//Se va a reservar memoria del tamaño de los términos del polinomio
+	//Se va a reservar memoria del tamaÃ±o de los tÃ©rminos del polinomio
 	a.datos = (float*)malloc(sizeof(float)*a.terminos);
 	return a;
 }
 
-//Esta función libera el espacio que se reservó con malloc
+//Esta funciÃ³n libera el espacio que se reservÃ³ con malloc
 void liberar(POLI *a){
 	free(a->datos);
 }
 
-/*La función leer pregunta al usuario el valor de cada elemento del polinomio
+/*La funciÃ³n leer pregunta al usuario el valor de cada elemento del polinomio
   y lo guarda en su respectivo lugar*/
 void leer(POLI *a){
 	for(int i = 0; i < a->terminos; i++){
@@ -33,7 +32,7 @@ void leer(POLI *a){
 	}
 }
 
-//Esta función imprime un polinomio incluyendo el x^n y tomando el cuenta el signo
+//Esta funciÃ³n imprime un polinomio incluyendo el x^n y tomando el cuenta el signo
 void imprimir(POLI a){
 	printf("%f x^%d ", a.datos[0], a.terminos - 1);
 	for(int i = 1; i < a.terminos; i++){
@@ -43,14 +42,14 @@ void imprimir(POLI a){
 	}
 }
 
-/*En esta función se suma cada término del polinomio y se guarda en su respectivo lugar
+/*En esta funciÃ³n se suma cada tÃ©rmino del polinomio y se guarda en su respectivo lugar
   del polinomio resultante*/
 void suma(POLI *a, POLI *b, POLI *c){
 	for(int i = 0; i < a->terminos; i++)
 		c->datos[i] = a->datos[i] + b->datos[i];
 }
 
-/*En esta función se resta cada término del polinomio y se guarda en su respectivo lugar
+/*En esta funciÃ³n se resta cada tÃ©rmino del polinomio y se guarda en su respectivo lugar
   del polinomio resultante*/
 void resta(POLI *a, POLI *b, POLI *c){
 	for(int i = 0; i < a->terminos; i++)
